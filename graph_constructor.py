a5564141e44a86e11207246257da9205124339c2
potatovich <filippovpetr2003@yandex.ru>
2024-08-08 16:32:58 +0500

    added exe and manual

--- graph_constructor.py
+++ graph_constructor.py
@@ -1,26 +1,29 @@
 import wfdb
 from other_methods import get_record, get_annotation, annotations_filter
+from tkinter import messagebox
 
 
 def show_graph(rec_name, req_sym, s_from, s_to): #Метод осуществляющий создание окна с графиком и выделенными в графе аннотациями
+    try:
+        record = get_record(rec_name, s_from, s_to)
+        annotation_atr = get_annotation(rec_name, s_from, s_to)
 
-    record = get_record(rec_name, s_from, s_to)
-    annotation_atr = get_annotation(rec_name, s_from, s_to)
+        fil_samples, fil_symbols = annotations_filter( #Вызов метода фильтрации аннотаций
+            anno_samp=annotation_atr.sample, 
+            anno_sym=annotation_atr.symbol,
+            req_sym=req_sym
+        )
 
-    fil_samples, fil_symbols = annotations_filter( #Вызов метода фильтрации аннотаций
-        anno_samp=annotation_atr.sample, 
-        anno_sym=annotation_atr.symbol,
-        req_sym=req_sym
-    )
-
-    wfdb.plot_items( #Построение графика + фильт. аннотаций
-        signal=record.p_signal, 
-        ann_samp=[fil_samples for _ in range(len(record.sig_name))],
-        ann_sym=[fil_symbols for _ in range(len(record.sig_name))],
-        title=rec_name,
-        figsize=(15, 7),
-        sharex=True,
-        sharey=True,
-    )
+        wfdb.plot_items( #Построение графика + фильт. аннотаций
+            signal=record.p_signal, 
+            ann_samp=[fil_samples for _ in range(len(record.sig_name))],
+            ann_sym=[fil_symbols for _ in range(len(record.sig_name))],
+            title=rec_name,
+            figsize=(15, 7),
+            sharex=True,
+            sharey=True,
+        )
+    except Exception:
+        messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе отрисовки графика!")
 
 # show_graph('databases/mit-bih-arrhythmia-database-1.0.0/100', ['[', ']', 'N'])
--- other_methods.py
+++ other_methods.py
@@ -2,71 +2,89 @@ import os
 import codecs
 import wfdb
 import numpy
+from tkinter import messagebox
 
 
-def get_paths():
+def get_paths(): #Метод, осуществляющий получение путей до всех файлов в подпапках папки databases
+    try:
+        paths = []
+        f_paths = []
+        
+        for folders, _, files in os.walk('databases'):
+            for fn in files:
+                paths.append('\\'.join([folders, fn]))
 
-    paths = []
-
-    for folders, _, files in os.walk('databases'):
-        for fn in files:
-            paths.append('\\'.join([folders, fn]))
-
-    return [path[:-4] for path in list(filter(lambda p: 
-                                            len(p.split('\\')) == 3 and 
-                                            '.atr' in p.split('\\')[2] and not 
-                                            '.atr-' in p.split('\\')[2], 
-                                            paths))]
+        for path in paths:
+            if '.atr' in path[len(path) - 4:] and not '.atr-' in path[len(path) - 4:]:
+                f_paths.append(path[:-4])
+        
+        return f_paths
+    except Exception:
+        messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе получения путей до всех файлов в подпапках папки databases!")
 
 def get_syms_and_descriptions(symbols): #Метод, осуществляющий получение списка всех доступных для данного графика аннотаций
+    try:
+        file = codecs.open('sym_label.txt', 'r', 'utf-8')
+        labels = file.readlines()
+        file.close()
 
-    file = codecs.open('sym_label.txt', 'r', 'utf-8')
-    labels = file.readlines()
-    file.close()
+        labels = [line.strip() for line in labels]
 
-    labels = [line.strip() for line in labels]
+        file = codecs.open('sym_description_rus.txt', 'r', 'utf-8')
+        descriptions = file.readlines()
+        file.close()
 
-    file = codecs.open('sym_description_rus.txt', 'r', 'utf-8')
-    descriptions = file.readlines()
-    file.close()
+        descriptions = [line.strip() for line in descriptions]
+        
+        labels_and_descriptions = [' '.join([l, d]) for l, d in zip(labels, descriptions) if l in symbols]
 
-    descriptions = [line.strip() for line in descriptions]
-    
-    labels_and_descriptions = [' '.join([l, d]) for l, d in zip(labels, descriptions) if l in symbols]
-
-    return labels_and_descriptions
+        return labels_and_descriptions
+    except Exception:
+        messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе получения доступных для данного сигнала аннотаций!")
 
 def annotations_filter(anno_samp, anno_sym, req_sym): #Метод осуществляющий фильтрацию аннотаций 
-    fil_sym = []
-    fil_samp = []
+    try:
+        fil_sym = []
+        fil_samp = []
 
-    for samp, sym in zip(anno_samp, anno_sym):
-        if sym in req_sym:
-            fil_sym.append(sym)
-            fil_samp.append(samp)
+        for samp, sym in zip(anno_samp, anno_sym):
+            if sym in req_sym:
+                fil_sym.append(sym)
+                fil_samp.append(samp)
 
-    return numpy.array(fil_samp[:]), fil_sym
+        return numpy.array(fil_samp[:]), fil_sym
+    except Exception:
+        messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе фильтрации аннотаций!")
 
 def get_record(file_name, s_from, s_to): #Метод осуществляющий получение данных о графике
+    try:
+        record = wfdb.rdrecord( #Считывание данных (для построения графика) из .hea и .dat файлов
+            record_name=file_name,
+            sampfrom=s_from,
+            sampto=s_to
+        )
 
-    record = wfdb.rdrecord( #Считывание данных (для построения графика) из .hea и .dat файлов
-        record_name=file_name,
-        sampfrom=s_from,
-        sampto=s_to
-    )
-
-    return record
+        return record
+    except Exception:
+            messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе получения данных о сигнале!")
 
 def get_annotation(file_name, s_from, s_to): #Метод осуществляющий получение данных об аннотации
+    try:
+        annotation_atr = wfdb.rdann( #Считывание данных (для построения аннотаций) из .atr файла(в некторых случаях вместо них идут .ari файлы, но в данном решении такие файлы игнорируются)
+            record_name=file_name, 
+            extension='atr',
+            sampfrom=s_from,
+            sampto=s_to,
+            shift_samps=True
+        )
 
-    annotation_atr = wfdb.rdann( #Считывание данных (для построения аннотаций) из .atr файла(в некторых случаях вместо них идут .ari файлы, но в данном решении такие файлы игнорируются)
-        record_name=file_name, 
-        extension='atr',
-        sampfrom=s_from,
-        sampto=s_to
-    )
-
-    return annotation_atr
+        return annotation_atr
+    except Exception:
+        messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе получения данных об аннотации!")
 
 def get_sig_len(file_name): #Метод осуществляющий получение длины записи
-    return wfdb.rdrecord(record_name=file_name).sig_len
+    try:
+        return wfdb.rdrecord(record_name=file_name).sig_len
+    except Exception:
+        messagebox.showerror(title="Запуск приостановлен", message="Возникла ошибка на этапе получения длины записи!")
+        
